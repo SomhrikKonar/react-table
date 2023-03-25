0f0eb0ecd0faaa6a04c4204c8ee374b5614d0543
Somhrik Konar <somhrikkonar1996@gmail.com>
2023-03-25 18:39:16 +0530

    minor change

--- src/components/Table/components/Header/View.tsx
+++ src/components/Table/components/Header/View.tsx
@@ -1,10 +1,23 @@
 import React from "react";
+import { useStore } from "../../../../store/store";
 import Filter from "./components/Filter";
 import Search from "./components/Search";
 import styles from "./styles.module.css";
 const View = () => {
+  const [{ searchAccessors, filters }] = useStore();
+  const filterOptions = React.useMemo(
+    () => Object.keys(filters).length,
+    [filters]
+  );
+
   return (
-    <div className={`${styles.container} tableHeaderContainer`}>
+    <div
+      className={`${styles.container} ${
+        searchAccessors.length > 0 || filterOptions > 0
+          ? "tableHeaderContainer"
+          : ""
+      }`}
+    >
       <Search />
       <Filter />
     </div>
